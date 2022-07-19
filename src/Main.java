9897d9d130ca47c726761b95cffdfd4d6ce6c199
Evgeniy Novichenko <tnrkip@yandex.ru>
2022-07-19 11:52:19 +0300

    initial srednee

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
         SalesManager unit_1= new SalesManager(new int[]{150,120,180});
         System.out.println(unit_1.max());
+        System.out.println(unit_1.min());
+        System.out.println(unit_1.srednee());
+
     }
 
 
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -6,12 +6,31 @@ public class SalesManager {
     }
 
     public int max() {
-        int max = -1;
+        int max = sales[0];
         for (int sale : sales) {
+
             if (sale > max) {
                 max = sale;
             }
         }
         return max;
     }
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+    public int srednee() {
+        int srednee=0;
+        for (int sale : sales) {
+            srednee+=sale;
+        }
+        return srednee-max()-min();
+    }
+
+
 }
