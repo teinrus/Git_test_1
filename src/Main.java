6cd78d938809b459204c01da1b4a2547b4ee649d
Evgeniy Novichenko <tnrkip@yandex.ru>
2022-07-19 11:30:30 +0300

    toLong

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        SalesManager unit_1= new SalesManager(new int[]{150,120,180});
+        SalesManager unit_1= new SalesManager(new long[]{150,120,180});
         System.out.println(unit_1.max());
     }
 
